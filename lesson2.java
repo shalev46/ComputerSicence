427fb5d27bde9705394c5b31ec01c494b6e715d4
shalev46 <154340729+shalev46@users.noreply.github.com>
2023-12-24 09:37:36 +0200

    Create lesson2.java

---
+++ lesson2.java
@@ -0,0 +1,75 @@
+/*
+class Main {
+  public static void main(String[] args) {
+int x=5;
+int y=6;
+int z=7;
+System.out.println(x);
+System.out.println(y);
+System.out.println(z);
+}
+ } 
+*/
+
+/* class Main {
+  public static void main(String[] args) {
+Float y= 3.4F;
+int x= 7;
+System.out.println(x+"   "+y);
+}
+ } */
+
+ /* class Main {
+  public static void main(String[] args) {
+char let1= 'n';
+char let2= 'o';
+char let3= 'a';  
+System.out.println (let1+""+let2+let3);
+}
+ }  */
+/* class Main {
+  public static void main(String[] args) {
+int x= 3;
+int z= 7;
+System.out.println(x+"is less than"+z);
+}
+  } */
+
+
+/* class Main {
+  public static void main(String[] args) {
+int x= 5;
+int y= 10;
+int z= 3;
+System.out.println((x + y + z)/3);
+}
+  } */
+
+
+
+/* class Main {
+  public static void main(String[] args) {
+int a= 4;
+int b= 3;
+int c= a*a+b*b; 
+System.out.println(c);
+}
+  } */
+
+/* class Main {
+  public static void main(String[] args) {
+int x= 6;
+int y= 2*x+5;
+System.out.println("y="+y);
+}
+  } */
+
+/* class Main {
+  public static void main(String[] args) {
+String s1 = "l like to study computer science";
+String s2 = "and therefore ready to invest";
+String s3 = "hours a day for pactice";
+int hours = 8;
+System.out.println("l like to study computer science " + "and therefore ready to invest " + hours + " " + "hours a day for pactice");
+}
+  }  */
