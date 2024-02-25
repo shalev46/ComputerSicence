6731a1d00ebc5be2c8aca4eb577927604cb45856
shalev46 <154340729+shalev46@users.noreply.github.com>
2024-02-25 11:29:53 +0200

    Update lesson2.java

--- lesson2.java
+++ lesson2.java
@@ -1,75 +1,39 @@
-/*
-class Main {
-  public static void main(String[] args) {
-int x=5;
-int y=6;
-int z=7;
-System.out.println(x);
-System.out.println(y);
-System.out.println(z);
-}
- } 
-*/
+package mekif_v;
 
-/* class Main {
-  public static void main(String[] args) {
-Float y= 3.4F;
-int x= 7;
-System.out.println(x+"   "+y);
-}
- } */
+import java.util.Scanner;
 
- /* class Main {
-  public static void main(String[] args) {
-char let1= 'n';
-char let2= 'o';
-char let3= 'a';  
-System.out.println (let1+""+let2+let3);
-}
- }  */
-/* class Main {
-  public static void main(String[] args) {
-int x= 3;
-int z= 7;
-System.out.println(x+"is less than"+z);
-}
-  } */
+public class class2 {
+	  
+	public static void f1() {
+		  int x = 5;
+          System.out.println(x);
+          int y = 10;
+          System.out.println(y);
+          int z = 15;
+          System.out.println(z);
+	  }
 
-
-/* class Main {
-  public static void main(String[] args) {
-int x= 5;
-int y= 10;
-int z= 3;
-System.out.println((x + y + z)/3);
-}
-  } */
-
-
-
-/* class Main {
-  public static void main(String[] args) {
-int a= 4;
-int b= 3;
-int c= a*a+b*b; 
-System.out.println(c);
-}
-  } */
-
-/* class Main {
-  public static void main(String[] args) {
-int x= 6;
-int y= 2*x+5;
-System.out.println("y="+y);
-}
-  } */
-
-/* class Main {
-  public static void main(String[] args) {
-String s1 = "l like to study computer science";
-String s2 = "and therefore ready to invest";
-String s3 = "hours a day for pactice";
-int hours = 8;
-System.out.println("l like to study computer science " + "and therefore ready to invest " + hours + " " + "hours a day for pactice");
-}
-  }  */
+	  public static void f2() {
+		  int x = 5;
+    	 float y = 1.5f;
+    	 System.out.println(x+"   "+y);
+       }
+	  
+	  public static void f3() {
+		  char char1 = 's';
+		  char char2 = 'h';
+		  char char3 = 'a';
+		  char char4 = 'l';
+		  char char5 = 'e';
+		  char char6 = 'v';
+		  System.out.println(char1+""+char2+""+char3+""+char4+""+char5+""+char6);
+	  }
+	  public static void f4() {
+		  int x = 4;
+		  int y = 5;
+		  System.out.println(x+" is less than "+y);
+	  }
+	  public static void main(String [] args) {
+		 f4();
+	  }
+  }
